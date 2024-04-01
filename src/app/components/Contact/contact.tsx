6eaefc2720d3e7f73691a00cd3735dfd2a0a9e17
vinicius-leonel <viniciuslps.cms@gmail.com>
2024-04-01 18:15:52 -0300

    corrigindo importação

--- src/app/components/Contact/contact.tsx
+++ src/app/components/Contact/contact.tsx
@@ -1,6 +1,5 @@
 import Image from "next/image";
 import Link from "next/link";
-import { FaLinkedin } from "react-icons/fa";
 
 
 export default function Contact () {
--- src/app/home.tsx
+++ src/app/home.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import Nav from "./components/Nav/nav"
+import Nav from "./components/Nav/Nav"
 
 export default function Home () {
     return (
