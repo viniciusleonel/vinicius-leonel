b13da8f6e0b2e5e5fec08163aba8c7fa1fa5d753
vinicius-leonel <viniciuslps.cms@gmail.com>
2024-04-01 15:54:01 -0300

    mudando skills para display grid

--- src/app/components/Skills/skills.tsx
+++ src/app/components/Skills/skills.tsx
@@ -2,9 +2,9 @@ import Image from "next/image";
 
 export default function Skills () {
     return (
-        <div className="bg-cyan-100 dark:bg-zinc-900 flex flex-1 flex-col justify-center  p-4 w-full border border-cyan-700 rounded-3xl">
-            <h3 className=" text-3xl text-center">Skills</h3>
-            <div className="flex justify-evenly  items-center flex-wrap w-full ">
+        <div className={` bg-cyan-100  dark:bg-zinc-900 flex flex-1 flex-col justify-center p-4 w-full border border-cyan-700 rounded-3xl `}>
+            <h3 className="text-2xl md:text-3xl text-center">Skills</h3>
+            <div className={`grid grid-cols-5 md:grid-cols-10 justify-center gap-4 items-center  w-full `}>
             
                 <Image 
                     src='/images/icons/html.svg'
@@ -58,12 +58,12 @@ export default function Skills () {
                     height={50}
                     width={50}
                 />
-                <Image 
+                {/* <Image 
                     src='/images/icons/kotlin.svg'
                     alt="teste"
                     height={50}
                     width={50}
-                />
+                /> */}
                 <Image 
                     src='/images/icons/java.svg'
                     alt="teste"
