b288045ccc650557e042ab21e815ec37351f51ab
vinicius-leonel <viniciuslps.cms@gmail.com>
2024-04-03 15:23:48 -0300

    teste de animacao mobile 2

--- src/app/components/Skills/skills.tsx
+++ src/app/components/Skills/skills.tsx
@@ -4,6 +4,7 @@ import { TechIcons } from "./techIcons"
 import { useState } from "react";
 
 export default function Skills () {
+
     const [isAnimationPaused, setIsAnimationPaused] = useState(false);
 
     const handleTouchStart = () => {
@@ -17,7 +18,7 @@ export default function Skills () {
     return (
         <div className={`overflow-hidden bg-cyan-100 dark:bg-zinc-900 border border-cyan-700 ${styles.skillsAnimationContainer}`}>
         <div
-            className={`flex gap-5 sm:gap-10 lg:gap-24 h-10 sm:h-20 my-2 sm:m-0 animate-smScreenMove sm:animate-mdScreenMove lg:animate-lgScreenMove ${styles.skillsAnimation}`}
+            className={`flex gap-5 sm:gap-10 lg:gap-24 h-10 sm:h-20 my-2 sm:m-0 ${isAnimationPaused ? 'pauseAnimation' : ''} animate-smScreenMove sm:animate-mdScreenMove lg:animate-lgScreenMove ${styles.skillsAnimation}`}
             onTouchStart={handleTouchStart}
             onTouchEnd={handleTouchEnd}
         >
