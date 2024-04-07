8a5c6c1b2b6e9becbde51bdef87480be47a4732a
vinicius-leonel <viniciuslps.cms@gmail.com>
2024-04-07 14:05:32 -0300

    testando carousel de projetos

--- src/app/components/Projects/projectList.tsx
+++ src/app/components/Projects/projectList.tsx
@@ -1,34 +1,59 @@
-import ProjectCard from "../../components/Projects/projectCard"
-import { JavaIcon, NodeIcon, SpringIcon } from "../../components/Skills/techIcons"
+import React, { useState } from 'react';
+import ProjectCard from "../../components/Projects/projectCard";
+import { JavaIcon, NodeIcon, SpringIcon } from "../../components/Skills/techIcons";
+import { FaArrowLeft, FaArrowRight } from "react-icons/fa";
 
-export function ProjectList () {
+export function ProjectList() {
+    const [currentIndex, setCurrentIndex] = useState(0);
+    const projects = [
+        {
+            imgCardSrc: "/images/cleanSky.jpg",
+            imgAlt: "teste",
+            title: "Title",
+            description: "Description Numquam minus quia nesciunt nemo explicabo magnam, iure accusamus iste delectus adipisci repellendus repellat fuga rem non impedit incidunt veniam. Nulla, autem.",
+            techIcons: [<JavaIcon key="java"/>, <SpringIcon key="spring"/>],
+            linkSite: "https://github.com/viniciusleonel",
+            linkGitHub: "https://github.com/viniciusleonel"
+        },
+        {
+            imgCardSrc: "",
+            imgAlt: "teste",
+            title: "Title",
+            description: "Description Ladipisicinghfkjxgjg ,cg,chckh.k fxhfhgrs rnsrnhdm elit. Numquam minus quia nesciunt nemo explicabo magnam, iure accusamus iste delectus adipisci repellendus repellat fuga rem non impedit incidunt veniam. Nulla, autem.",
+            techIcons: [<NodeIcon key="node"/>, <SpringIcon key="spring"/>],
+            linkSite: "https://github.com/viniciusleonel",
+            linkGitHub: "https://github.com/viniciusleonel"
+        }
+    ];
+
+    const nextProject = () => {
+        setCurrentIndex((prevIndex) => (prevIndex + 1) % projects.length);
+    };
+
+    const prevProject = () => {
+        setCurrentIndex((prevIndex) => (prevIndex === 0 ? projects.length - 1 : prevIndex - 1));
+    };
+
     return (
         <div className=" h-svh flex flex-col justify-center items-center">
-                <h2 className="-mt-20 pb-14 text-3xl">Projects</h2>
-                <div className="flex overflow-hidden  w-2/3 gap-6 justify-center">
-                    <ProjectCard
-                        imgCardSrc="/images/cleanSky.jpg" 
-                        imgAlt="teste"
-                        title="Title"
-                        description="Description Numquam minus quia nesciunt nemo explicabo magnam, iure accusamus iste delectus adipisci repellendus repellat fuga rem non impedit incidunt veniam. Nulla, autem."
-                        techIcons={[<JavaIcon key="java"/>, <SpringIcon key="spring"/>] }
-                        linkSite="https://github.com/viniciusleonel"
-                        linkGitHub="https://github.com/viniciusleonel"
-                    />
-                    {/* <ProjectCard
-                        imgCardSrc=""
-                        imgAlt="teste" 
-                        title="Title"
-                        description="Description 
-                        Ladipisicinghfkjxgjg
-                        
-                        ,cg,chckh.k fxhfhgrs rnsrnhdm elit. Numquam minus quia nesciunt nemo explicabo magnam, iure accusamus iste delectus adipisci repellendus repellat fuga rem non impedit incidunt veniam. Nulla, autem."
-                        techIcons={[<NodeIcon key="node"/>, <SpringIcon key="spring"/>] }
-                        linkSite="https://github.com/viniciusleonel"
-                        linkGitHub="https://github.com/viniciusleonel"
-                    /> */}
-                    
-                </div>
+            <h2 className="-mt-20 pb-14 text-3xl">Projects</h2>
+            <div className="flex overflow-hidden  w-2/3 gap-6 justify-center">
+                <button onClick={prevProject}><FaArrowLeft /></button>
+                {projects.map((project, index) => (
+                    <ul key={index} style={{ display: index === currentIndex ? 'block' : 'none' }}>
+                        <ProjectCard
+                            imgCardSrc={project.imgCardSrc}
+                            imgAlt={project.imgAlt}
+                            title={project.title}
+                            description={project.description}
+                            techIcons={project.techIcons}
+                            linkSite={project.linkSite}
+                            linkGitHub={project.linkGitHub}
+                        />
+                    </ul>
+                ))}
+                <button onClick={nextProject}><FaArrowRight /></button>
             </div>
-    )
+        </div>
+    );
 }
