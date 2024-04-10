1e8ef9888806f85736c8c16664159993f0396af4
vinicius-leonel <viniciuslps.cms@gmail.com>
2024-04-10 18:39:22 -0300

    alterando informacoes no componente about e inserindo link de whatsapp

--- src/app/components/About/about.tsx
+++ src/app/components/About/about.tsx
@@ -5,15 +5,15 @@ import { lusitana } from '@/app/ui/fonts';
 
 export default function About () {
     return (
-        <div className="mt-0 xs:mt-16 mx-8 lg:mx-20 dark:bg-dark-secondary border-2 border-cyan-700 dark:border-cyan-500 rounded-3xl">
+        <div className="w-full mt-0 xs:mt-16 mx-8 lg:mx-20 dark:bg-dark-secondary border-2 border-cyan-700 dark:border-cyan-500 rounded-3xl">
                 <div className="mt-10 text-2xl md:text-3xl flex items-center justify-center w-full">
                     <span className="text-cyan-500 text-4xl">&#123;</span>
                     <h2 className="pt-1"> HELLO WORLD </h2>
                     <span className="text-cyan-500 text-4xl">&#125;</span>
                 </div>
             <div className="p-4 sm:px-8 md:px-10 lg:px-14 flex flex-col sm:flex-row items-center justify-center  w-full">
-                <div>
-                    <div className=" w-full">
+                <div className="w-full ">
+                    <div className="">
                         <div className=" flex flex-col md:flex-row items-baseline">
                             <p className="dark:text-cyan-100">My name is </p>
                             <h1 className=" text-2xl sm:text-3xl md:text-4xl lg:text-5xl bg-clip-text text-transparent bg-gradient-to-r from-cyan-400 to-blue-600"> Vinícius Leonel</h1>
@@ -23,12 +23,15 @@ export default function About () {
                             <h2 className=" text-2xl sm:text-3xl md:text-4xl lg:text-5xl bg-clip-text text-transparent bg-gradient-to-r from-cyan-400 to-blue-600">Full Stack Developer</h2>
                         </div>
                     </div>
-                    <p className={`${lusitana.className} mt-6 sm:w-3/4 w-full text-sm md:text-base text-zinc-800 dark:text-zinc-300`}>
-                        Currently studying System Development and Analysis at FIAP + Alura. ( Java, OOP, Spring Boot and Kotlin. )
+                    <p className={`${lusitana.className} mt-6 text-sm md:text-base text-zinc-800 dark:text-zinc-300`}>
+                        Currently in the last year course of System Development and Analysis at FIAP + Alura. 
+                        <br />
+                        <span className="bg-clip-text text-transparent bg-gradient-to-r from-cyan-400 to-blue-600">Main skills: Java, Spring, NodeJs, NextJs, TypeScript, React, English.</span>
+                        <br />
                         Living in São Paulo and looking for an opportunity to further develop my skills.
                     </p>
                     <div className="flex justify-between mt-4 ">
-                        <div className="flex items-center -m-2 pt-6">
+                        <div className="flex items-center gap-2 w-32 lg:w-40 -m-2 pt-6">
                             <Link
                                 href="https://www.linkedin.com/in/viniciuslps/"
                             >
@@ -50,9 +53,20 @@ export default function About () {
                                 width={50}
                             />
                             </Link>
+
+                            <Link
+                                href=" https://w.app/ViniciusLeonel"
+                            >
+                            <Image 
+                                src='/images/icons/whatsapp.svg'
+                                alt="whatsapp icon"
+                                height={50}
+                                width={50}
+                            />
+                            </Link>
                         </div>
 
-                        <div className="me-6 sm:hidden">
+                        <div className="me-6 md:hidden">
                             <Image 
                                 src="/images/vinicius.jpeg"
                                 height={100}
@@ -66,7 +80,7 @@ export default function About () {
                     
                 </div>
 
-                <div className="me-6 mb-10 hidden sm:flex">
+                <div className="me-6 mb-10 hidden md:flex ">
                     <Image 
                         src="/images/vinicius.jpeg"
                         height={100}
--- src/app/components/Skills/skills.tsx
+++ src/app/components/Skills/skills.tsx
@@ -6,7 +6,7 @@ import { useState } from "react";
 export default function Skills () {
 
     return (
-        <div className={`mb-1 overflow-hidden bg-cyan-100 dark:bg-zinc-900 border-2 border-cyan-700 ${styles.skillsAnimationContainer}`}>
+        <div className={`mb-1 overflow-hidden dark:bg-zinc-900 border-2 border-cyan-700 ${styles.skillsAnimationContainer}`}>
         <div
             className={`flex gap-5 sm:gap-10 lg:gap-24 h-10 sm:h-20 my-2 sm:m-0 animate-xsScreenMove sm:animate-smScreenMove md:animate-mdScreenMove lg:animate-lgScreenMove ${styles.skillsAnimation}`}
 
