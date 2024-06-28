38149b4d8ae77d0c97554e3cac43512e94b3e806
vinicius-leonel <viniciuslps.cms@gmail.com>
2024-06-28 00:41:12 -0300

    fix: corrigindo erro de importacao em componente shadcnui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,21 @@
-'use client'
+"use client";
 
-import Nav from "./components/Nav/Nav"
-import Skills from "./components/Skills/skills"
-import About from "./components/About/about"
-import  {ProjectCarousel}  from "./components/Projects/project-carousel"
-import { useContext, useEffect } from "react"
-import { AuthContext } from "./context/AuthContext"
-import ExpecienciasPage from "./components/Expeciencias/page"
+import Nav from "./components/Nav/Nav";
+import About from "./components/About/about";
+import ExpecienciasPage from "./components/Expeciencias/page";
+import { ProjectCarousel } from "./components/Projects/project-carousel";
+import { useContext, useEffect } from "react";
+import { AuthContext } from "./context/AuthContext";
 
-export default function Home () {
-
-    const {signIn} = useContext(AuthContext)
+export default function Home() {
+    const { signIn } = useContext(AuthContext);
 
     useEffect(() => {
         const signInAsync = async () => {
             try {
                 const data = {
-                    email: '',
-                    password: ''
+                    email: "",
+                    password: "",
                 };
                 await signIn(data);
             } catch (error) {
@@ -29,26 +27,29 @@ export default function Home () {
         signInAsync();
     }, []); // Empty array ensures this runs only once
 
-    return (      
-    <>
-        <nav className="mb-5 sm:mb-0">
-            <Nav />
-        </nav>
-        <main className="bg-[#f5f9fb] dark:bg-dark-secondary">
-            <section className="min-h-screen flex items-center ">
-                <About />    
-            </section> 
+    return (
+        <>
+            <nav className="mb-5 sm:mb-0">
+                <Nav />
+            </nav>
+            <main className="bg-[#f5f9fb] dark:bg-dark-secondary">
+                <section className="min-h-screen flex items-center ">
+                    <About />
+                </section>
 
-            <section className="min-h-screen flex items-center bg-[#f5f9fb] dark:bg-dark-secondary ">
-                <ExpecienciasPage />
-            </section>
+                <section className="min-h-screen flex items-center bg-[#f5f9fb] dark:bg-dark-secondary ">
+                    <ExpecienciasPage />
+                </section>
 
-            <section id="Projects" className="min-h-screen pt-16">
-                <ProjectCarousel />
-            </section>  
-            
-            {/* <Skills />     */}
-        </main>    
-    </>
-    )
+                <section
+                    id="Projects"
+                    className="min-h-screen pt-16"
+                >
+                    <ProjectCarousel />
+                </section>
+
+                {/* <Skills />     */}
+            </main>
+        </>
+    );
 }
